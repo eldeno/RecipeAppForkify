acaac4d10d3b31b40962e53c646fe1facc78d050
eldeno <chagasag@hotmail.com>
2018-06-03 12:15:20 -0300

    Aula 129
    Search model

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,6 @@
 
-import axios from 'axios';
+import Search from './models/Search';
 
-async function getResults(query) {
-    const key = '903cd9fe3295f590b0c32f33e846d8d4';
-    const proxy = 'https://cors-anywhere.herokuapp.com/';
-    try {
-        const res = await axios(`${proxy}http://food2fork.com/api/search?key=${key}&q=${query}`);
-        const recipes = res.data.recipes;
-        console.log(recipes);
-    } catch(error) {
-        alert(error);
-    }   
-}
-getResults('tomato pasta');
-
-// http://food2fork.com/api/search 
-// 903cd9fe3295f590b0c32f33e846d8d4
+const search = new Search('pizza');
+console.log(search);
+search.getResults();
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,1 +1,20 @@
-export default 'I am an exported string.';
+import axios from 'axios';
+
+export default class Search {
+    constructor(query) {
+        this.query = query;
+    }
+
+    async getResults() {
+        const key = '903cd9fe3295f590b0c32f33e846d8d4';
+        const proxy = 'https://cors-anywhere.herokuapp.com/';
+        try {
+            const res = await axios(`${proxy}http://food2fork.com/api/search?key=${key}&q=${this.query}`);
+            this.result = res.data.recipes;
+            console.log(this.result);
+        } catch(error) {
+            alert(error);
+        }   
+    }
+}
+
