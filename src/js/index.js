db52f35ad5edcc676574619cb43c28997db90ea5
eldeno <chagasag@hotmail.com>
2018-06-09 19:18:24 -0300

    Aula 134
    Adding page buttons to move through the search results

--- src/js/index.js
+++ src/js/index.js
@@ -38,3 +38,12 @@ elements.searchForm.addEventListener('submit', e => {
     controlSearch();
 });
 
+elements.searchResPages.addEventListener('click', e => {
+    const btn = e.target.closest('.btn-inline');
+    if (btn) {
+        const goToPage = parseInt(btn.dataset.goto, 10); // 10 is related to the base for conversion //// REVIEW THIS
+        searchView.clearResults();
+        searchView.renderResults(state.search.result, goToPage);
+    }
+});
+
