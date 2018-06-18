e72ead5461cf610c71f0c0059f3ab7b9ca3f7a68
eldeno <chagasag@hotmail.com>
2018-06-18 19:10:26 -0300

    Aula 139 (Fractional problem)
    Recipe View Part 2
    Fractional is not working properly

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -9,6 +9,15 @@ export const clearInput = () => {
 export const clearResults = () => {
     elements.searchResList.innerHTML = '';
     elements.searchResPages.innerHTML = '';
+};
+
+export const highlightSelected = id => {
+    const resultsArr = Array.from(document.querySelectorAll('.results__link'));
+    resultsArr.forEach(el => {
+        el.classList.remove('result__link--active');
+    });
+
+    document.querySelector(`a[href="#${id}"]`).classList.add('result__link--active'); // selects the link (css "a" selector) with the id
 }
 
 /*
@@ -40,7 +49,7 @@ const limitRecipeTitle = (title, limit = 17) => {
 const renderRecipe = recipe => {
     const markup = `
         <li>
-            <a class="results__link results__link--active" href="#${recipe.recipe_id}">
+            <a class="results__link" href="#${recipe.recipe_id}">
                 <figure class="results__fig">
                     <img src="${recipe.image_url}" alt="${recipe.title}">
                 </figure>
